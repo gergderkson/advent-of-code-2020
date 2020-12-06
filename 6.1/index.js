cb7bfd7799dd43303fba0d953326315c5db4eb95
Greg Dickson <gregdickson17@gmail.com>
2020-12-06 11:05:29 +0000

    day 6 part 1

--- 6.1/index.js
+++ 6.1/index.js
@@ -3,8 +3,7 @@ const main = (input) => {
 	return input
 		.split("\n\n")
 		.map(v => v.replace(/\n/g, ""))
-		.map(v => new Set(v.split("")))
-		.map(v => v.size)
+		.map(v => new Set(v.split("")).size)
 		.reduce((c, v) => c + v);
 };
 module.exports = main;
